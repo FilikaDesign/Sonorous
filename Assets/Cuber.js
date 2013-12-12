15f99f7a1a4bd47cd9af79021c80013303a354c1
selcukartut <selcukartut@gmail.com>
2013-12-12 14:27:13 +0200

    Sonorous v4

---
+++ Assets/Cuber.js
@@ -0,0 +1,226 @@
+#pragma strict
+
+// Import Array List Class
+// Reference http://forum.unity3d.com/threads/69281-Resizeable-array-for-javascript-Arraylist
+import System.Collections.Generic;
+
+
+
+private var wall 		: GameObject;
+private var floor 		: GameObject;
+
+private var mouseScreen : Vector3 = new Vector3(0,0,0);
+private var mouseWorld 	: Vector3 = new Vector3(0,0,0);
+private var offSet 		: Vector3 = new Vector3(0,0,0);
+	
+
+private var woodThickness : float = 1;
+
+private var parameters : List.<Hashtable> = new List.<Hashtable>();
+private var moduls : List.<GameObject> = new List.<GameObject>();
+
+function Start () {
+	
+	//camera positioning
+	this.transform.position = Vector3(90,150,-250);
+	this.transform.Rotate(15,-5,0);
+	
+	// Make a game object
+	
+	var lightGameObject : GameObject = new GameObject("The Light");
+	
+	// Add the light component
+	lightGameObject.AddComponent(Light);
+	// Set color and position
+	lightGameObject.light.color = Color.white;
+	lightGameObject.light.type = LightType.Directional;
+	lightGameObject.light.intensity = 0.4;
+	// Set the position (or any transform property) after
+	// adding the light component.
+	lightGameObject.transform.position = Vector3(0, 500, 0);
+	lightGameObject.transform.Rotate(40, 5, 200);
+	
+	//point light
+	var pointLightGameObject : GameObject = new GameObject("The Point One");
+	
+	// Add the light component
+	pointLightGameObject.AddComponent(Light);
+	// Set color and position
+	pointLightGameObject.light.color = Color.white;
+	pointLightGameObject.light.type = LightType.Point;
+	pointLightGameObject.light.intensity = 1;
+	pointLightGameObject.light.range = 130;
+	// Set the position (or any transform property) after
+	// adding the light component.
+	pointLightGameObject.transform.position = Vector3(60, 140, -31);
+	pointLightGameObject.transform.Rotate(40, 5, 200);
+	
+	
+	//create Background Wall and Floor
+	wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+	wall.name = "Wall";
+	var wallBoxCollider : BoxCollider = wall.GetComponent("BoxCollider");
+	wallBoxCollider.enabled = false;
+	wall.transform.position = Vector3(0,150,30);
+	wall.transform.localScale = Vector3(800,2,300);
+	wall.transform.Rotate(90,0,0);
+	wall.renderer.material.mainTexture = Resources.Load("wall", Texture2D);
+	wall.renderer.material.mainTextureScale = Vector2 (11,11);
+	
+	//floor
+	floor = GameObject.CreatePrimitive(PrimitiveType.Cube);
+	floor.name = "Floor";
+	var floorBoxCollider : BoxCollider = floor.GetComponent("BoxCollider");
+	floorBoxCollider.enabled = false;
+	floor.transform.position = Vector3(0,0,-120);
+	floor.transform.localScale = Vector3(800,2,300);
+	floor.transform.Rotate(0,0,0);
+	floor.renderer.material.mainTexture = Resources.Load("wooden-floor-texture", Texture2D);
+	floor.renderer.material.mainTextureScale = Vector2 (11,11);
+
+	
+	//create Parameter Data
+	var myStuffTex:Hashtable = {"Front":"H3375_ST22",
+                        "FrontUp":"200s",
+                        "FrontDown":"200s",
+                        "Back":"H3375_ST22",
+                        "Left":"H3375_ST22",
+                        "Right":"H3375_ST22",
+                        "Bottom":"H3375_ST22",
+                        "Top":"200s",
+                        "Hole":0,
+                        "nFrontFace":2,
+                        "w":70,
+                        "h":40,
+                        "depth":50,
+                        "x":0,
+                        "y":0,
+                        "isRigid":1
+                        };
+                        
+    var myStuffTex2:Hashtable = {"Front":"H3375_ST22",
+                        "FrontUp":"200s",
+                        "FrontDown":"200s",
+                        "Back":"H3375_ST22",
+                        "Left":"H3375_ST22",
+                        "Right":"H3375_ST22",
+                        "Bottom":"H3375_ST22",
+                        "Top":"200s",
+                        "Hole":1,
+                        "nFrontFace":1,
+                        "w":120,
+                        "h":40,
+                        "depth":50,
+                        "x":98,
+                        "y":0,
+                        "isRigid":1
+                        };
+                        
+    var myStuffTex3:Hashtable = {"Front":"H3375_ST22",
+                        "FrontUp":"200s",
+                        "FrontDown":"200s",
+                        "Back":"H3375_ST22",
+                        "Left":"H3375_ST22",
+                        "Right":"H3375_ST22",
+                        "Bottom":"H3375_ST22",
+                        "Top":"200s",
+                        "Hole":0,
+                        "nFrontFace":2,
+                        "w":40,
+                        "h":80,
+                        "depth":50,
+                        "x":178,
+                        "y":0,
+                        "isRigid":1
+                        };
+    //////
+           
+	//Add parameter data to ArrayList
+	parameters.Add(myStuffTex);
+	parameters.Add(myStuffTex2);
+	parameters.Add(myStuffTex3);
+	/////
+	
+	
+	// Create moduls 1
+	var eleman : GameObject = new GameObject("Kutu");
+	eleman.AddComponent("Element");
+	
+	
+	var other : Element = eleman.GetComponent("Element");
+	other.params = parameters[0];
+	
+	/////// 2
+	var eleman2 : GameObject = new GameObject("Kutu2");
+	eleman2.AddComponent("Element");
+	
+	
+	var other2 : Element = eleman2.GetComponent("Element");
+	other2.params = parameters[1];
+
+
+	//////// 3
+	var eleman3 : GameObject = new GameObject("Kutu3");
+	eleman3.AddComponent("Element");
+	
+	var other3 : Element = eleman3.GetComponent("Element");
+	other3.params = parameters[2];
+	
+
+}
+
+function Update () {
+	var mainCamera = Camera.main;
+	var hit : RaycastHit;
+
+	if (Input.GetMouseButtonDown (0)){
+	
+		if( Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition),  hit ) ) {
+		
+		
+			
+			mouseScreen = Vector3(Input.mousePosition.x,Input.mousePosition.y,-1 * mainCamera.transform.position.z);
+			
+			mouseWorld = mainCamera.ScreenToWorldPoint(mouseScreen);
+			
+			offSet = mouseWorld-hit.transform.position;		
+		
+		    
+		}
+			
+	
+	}
+	
+	if (Input.GetMouseButton (0)){
+	
+		if( Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition),  hit ) ) {
+			
+			mouseScreen = Vector3(Input.mousePosition.x,Input.mousePosition.y,-1 * mainCamera.transform.position.z);
+			
+			mouseWorld = mainCamera.ScreenToWorldPoint(mouseScreen);
+			
+			//Debug.Log(hit.rigidbody.gameObject.GetComponent("BoxCollider"));
+				
+			if(hit.rigidbody.gameObject.GetComponent("BoxCollider")){
+			
+			
+				var oLayer1 : 	GameObject = hit.collider.gameObject;
+				var oLayer2	:	GameObject = oLayer1.transform.parent.gameObject;
+				
+				var variableScript : Element;
+	 	 		variableScript = oLayer2.GetComponent("Element");
+	 	 		
+				//Debug.Log(variableScript.xPos);
+	 	 		
+	 	 
+		    	hit.transform.position.x = mouseWorld.x - offSet.x;
+				hit.transform.position.y = mouseWorld.y - offSet.y;
+				hit.transform.position.z = 0;	    	
+				
+	    	}
+	   
+		}
+	}
+	
+	 
+}
---
+++ Assets/Element.js
@@ -0,0 +1,174 @@
+#pragma strict
+
+var elementContainer: GameObject;
+var cubeFront 		: GameObject;
+var cubeFrontUp		: GameObject;
+var cubeFrontDown	: GameObject;
+var cubeBack 		: GameObject;
+var cubeLeft 		: GameObject;
+var cubeRight 		: GameObject;
+var cubeBottom 		: GameObject;
+var cubeTop 		: GameObject;
+var cubeHole 		: GameObject;
+var woodThickness 	: float = 1;
+
+var xPos				: int = 0;
+var yPos				: int = 0;
+var params:Hashtable = {};
+
+function Start (){
+    	
+
+    		elementContainer = new GameObject("Element Container");
+    		elementContainer.transform.parent = this.transform;
+    		
+			
+			////
+    		
+    		var h:float = params["h"];
+			var w:float = params["w"];
+			var depth:float = params["depth"];
+			var x:float = params["x"];
+			var y:float = params["y"];
+			
+			elementContainer.transform.parent.localPosition.y = h*0.5;
+
+
+			//collider
+			//Debug.Log(this.gameObject);
+			
+			var sc : BoxCollider;
+			sc = elementContainer.gameObject.AddComponent ("BoxCollider");
+			
+			elementContainer.GetComponent("BoxCollider").transform.position.y = h * 0.5;
+			
+			
+			sc.transform.localScale = Vector3(w,h*1.1,depth);
+			
+			sc.isTrigger = true;
+			//sc.convex = true;
+			
+			var Front:String = params["Front"]; 
+			var FrontUp:String = params["FrontUp"]; 
+			var FrontDown:String = params["FrontDown"]; 
+			var Back:String = params["Back"]; 
+			var Left:String = params["Left"]; 
+			var Right:String = params["Right"];
+			var Bottom:String = params["Bottom"];
+			var Top:String = params["Top"];
+			var hole:int = params["Hole"];
+			var isRigid:int =  params["isRigid"];
+			
+			if(isRigid == 1) {
+				elementContainer.AddComponent(Rigidbody);
+				elementContainer.rigidbody.isKinematic = true;
+				elementContainer.rigidbody.useGravity = false;
+			}
+			
+			
+			if(params["nFrontFace"] == 1){
+			
+			
+			
+			//Front
+		    cubeFront  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		    cubeFront.transform.localScale = Vector3(w,h,woodThickness);
+			cubeFront.transform.localPosition = Vector3(0,h * 0.5, -1 * depth * 0.5 + woodThickness*0.5);
+			cubeFront.renderer.material.mainTexture = Resources.Load(Front, Texture2D);
+			cubeFront.transform.parent = elementContainer.transform;
+			
+			
+			}else if(params["nFrontFace"] == 2){
+			
+			
+			//FrontUp
+		    cubeFrontUp  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		    cubeFrontUp.transform.localScale = Vector3(w,h*0.5-woodThickness*0.2,woodThickness);
+			cubeFrontUp.transform.localPosition = Vector3(0,h * 0.75, -1 * depth * 0.5 + woodThickness*0.5);
+			cubeFrontUp.renderer.material.mainTexture = Resources.Load(FrontUp, Texture2D);
+			cubeFrontUp.transform.parent = elementContainer.transform;
+			//FrontDown
+			cubeFrontDown  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		    cubeFrontDown.transform.localScale = Vector3(w,h*0.5-woodThickness*0.2,woodThickness);
+			cubeFrontDown.transform.localPosition = Vector3(0,h * 0.25, -1 * depth * 0.5 + woodThickness*0.5);
+			cubeFrontDown.renderer.material.mainTexture = Resources.Load(FrontDown, Texture2D);
+			cubeFrontDown.transform.parent = elementContainer.transform;
+			
+			}
+			
+			//Back
+		    cubeBack  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		    cubeBack.transform.localScale = Vector3(w,h,woodThickness);
+			cubeBack.transform.localPosition = Vector3(0,h * 0.5, depth * 0.5 - woodThickness*0.5);
+			cubeBack.renderer.material.mainTexture = Resources.Load(Back, Texture2D);
+			cubeBack.transform.parent = elementContainer.transform;
+			
+			//Left
+		    cubeLeft  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		    cubeLeft.transform.localScale = Vector3(woodThickness,h,depth);
+			cubeLeft.transform.localPosition = Vector3(-1 * w * 0.5 + woodThickness*0.5,h * 0.5, 0);
+			cubeLeft.renderer.material.mainTexture = Resources.Load(Left, Texture2D);
+			cubeLeft.transform.parent = elementContainer.transform;
+			
+			//Right
+		    cubeRight  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		    cubeRight.transform.localScale = Vector3(woodThickness,h,depth);
+			cubeRight.transform.localPosition = Vector3(w * 0.5 - woodThickness*0.5,h * 0.5, 0);
+			cubeRight.renderer.material.mainTexture = Resources.Load(Right, Texture2D);
+			cubeRight.transform.parent = elementContainer.transform;
+			
+			//Bottom
+		    cubeBottom  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		    cubeBottom.transform.localScale = Vector3(w,woodThickness,depth);
+			cubeBottom.transform.localPosition = Vector3(0,woodThickness*0.5, 0);
+			cubeBottom.renderer.material.mainTexture = Resources.Load(Bottom, Texture2D);
+			cubeBottom.transform.parent = elementContainer.transform;
+			
+			//Top
+		    cubeTop  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		    cubeTop.transform.localScale = Vector3(w,woodThickness,depth);
+			cubeTop.transform.localPosition = Vector3(0,h, 0);
+			cubeTop.renderer.material.mainTexture = Resources.Load(Top, Texture2D);
+			//cubeTop.renderer.material.color = Color.gray;
+			cubeTop.transform.parent = elementContainer.transform;
+			
+			
+			if(hole ==  1){
+			//Hole
+		    cubeHole  = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		    cubeHole.transform.localScale = Vector3(10,0,3);
+			cubeHole.transform.localPosition = Vector3(0,h+1, depth*0.5-1.8);
+			cubeHole.renderer.material.color = Color.gray;
+			cubeHole.transform.parent = elementContainer.transform;
+			
+			}
+			
+			Place(x,y);
+			
+    	}
+    	
+    	
+    	
+function Place (_xPos : int, _yPos : int)
+{
+	 	xPos = _xPos;
+ 		yPos = _yPos;
+ 		elementContainer.transform.localPosition = Vector3(xPos,yPos, 0);
+ 		
+
+}
+
+
+function Update ()
+{
+
+//Debug.Log("ju");
+	
+}
+
+function OnCollisionEnter(collision : Collision) {
+		// Debug-draw all contact points and normals
+		Debug.Log("ju");
+		
+}
+    	
